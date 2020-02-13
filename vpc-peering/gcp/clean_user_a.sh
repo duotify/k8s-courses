f25e9b1ab080d7e02f013c7f66ee28046e5aad68
Jeffrey Chu <peihuachu1112@gmail.com>
2020-02-13 08:31:22 +0000

    add vpc peering clean up scripts
    Signed-off-by: Jeffrey Chu <peihuachu1112@gmail.com>

---
+++ vpc-peering/gcp/clean_user_a.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z $MY_PROJECT_ID ]]; then
+  echo "project id not set"
+  exit 1
+fi
+
+gcloud config set project $MY_PROJECT_ID
+gcloud config set compute/region asia-east1
+gcloud config set compute/zone asia-east1-b
+
+gcloud compute instances delete k8s-node01 -q
+gcloud compute networks peerings delete peer-to-node02 --network k8snet -q
+gcloud compute firewall-rules delete k8snet-allow-common -q
+gcloud compute firewall-rules delete k8snet-allow-internal -q
+gcloud compute networks subnets delete k8snet-asiaeast1 -q
+gcloud compute networks delete k8snet -q
