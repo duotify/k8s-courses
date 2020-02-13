4d54894f350f48212f0a8bb7ceab6d00b3dff290
Jeffrey Chu <peihuachu1112@gmail.com>
2020-02-13 10:48:14 +0000

    Fix typo 2
    Signed-off-by: Jeffrey Chu <peihuachu1112@gmail.com>

--- lab2/download-kubectl.sh
+++ lab2/download-kubectl.sh
@@ -7,5 +7,5 @@ mv kubectl bin/kubectl
 chmod +x ~/bin/kubectl
 
 ~/bin/kubectl completion bash | tee ~/.bash_completion > /dev/null
-echo 'alias k=kubectl' | tee ~/.bash_aliases /dev/null
+echo 'alias k=kubectl' | tee ~/.bash_aliases > /dev/null
 echo 'complete -F __start_kubectl k' | tee -a ~/.bash_aliases > /dev/null
