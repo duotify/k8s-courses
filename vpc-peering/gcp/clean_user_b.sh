d1a6ea6ceddc44747f5b6562bbacd4a0be2ee4aa
Jeffrey Chu <peihuachu1112@gmail.com>
2020-02-14 11:43:04 +0000

    fix clean up script
    Signed-off-by: Jeffrey Chu <peihuachu1112@gmail.com>

--- vpc-peering/gcp/clean_user_b.sh
+++ vpc-peering/gcp/clean_user_b.sh
@@ -9,8 +9,8 @@ gcloud config set project $MY_PROJECT_ID
 gcloud config set compute/region asia-east1
 gcloud config set compute/zone asia-east1-b
 
-gcloud compute instances delete k8s-node01 -q
-gcloud compute networks peerings delete peer-to-node02 --network k8snet -q
+gcloud compute instances delete k8s-node02 -q
+gcloud compute networks peerings delete peer-to-node01 --network k8snet -q
 gcloud compute firewall-rules delete k8snet-allow-common -q
 gcloud compute firewall-rules delete k8snet-allow-internal -q
 gcloud compute networks subnets delete k8snet-asiaeast1 -q
